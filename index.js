e54d134fdbda68bb049ee13291001c60c1adb62b
ANUJ KUMAR YADAV <anujf0510@gmail.com>
2021-08-20 18:49:27 +0530

    fix bug

--- index.js
+++ index.js
@@ -12,18 +12,12 @@ function errorMsg(msg) {
     errorMessage.innerText = msg;
 }
 
-function calculateAmount(amountToReturned) {
-    for (let index = 0; index < availableNote.length; index++) {
-        let noOfNoteReturned = Math.trunc(amountToReturned / availableNote[index]);
-        amountToReturned %= availableNote[index];
-        noOfNotes[index].innerText = noOfNoteReturned;
-    }
-}
 nextBtn.addEventListener("click", () => {
 
     if (Number(billAmount.value > 0)) {
         errorMessage.style.display = "none";
         hide.style.display = "block";
+        nextBtn.style.display ="none";
 
     } else {
         errorMsg("Please enter valid number");
@@ -31,16 +25,33 @@ nextBtn.addEventListener("click", () => {
 });
 
 checkBtn.addEventListener("click", () => {
+    errorMessage.style.display = "none";
+    clearNoOfNotes();
     if (billAmount.value > 0) {
         if (cashPaid.value > billAmount.value) {
-            let amountToReturned = cashPaid.value - billAmount.value;
-            calculateAmount(amountToReturned);
-        } else {
+            let amountToReturned = Number(cashPaid.value - billAmount.value);
+            console.log(amountToReturned);
+            calculateNote(amountToReturned)
+        }
+        else {
             errorMessage.style.display = "block";
             errorMsg("Cash is less than bill, please enter right amount");
-
         }
     } else {
         errorMsg("Please enter valid number");
     }
 });
+
+function clearNoOfNotes(){
+    for(noOfNoteReturned of noOfNotes){
+        noOfNoteReturned.innerText = "";
+    }
+}
+
+function calculateNote(amountToReturned){
+    for (let index = 0; index < availableNote.length; index++) {
+        let noOfNoteReturned = Math.trunc(amountToReturned / availableNote[index]);
+        amountToReturned %= availableNote[index];
+        noOfNotes[index].innerText = noOfNoteReturned;
+    }
+}
